cddcf578c42d314339bcd4196c42aa6903d8dbe8
smurray-mvla <scott.murray@mvla.net>
2023-01-31 07:59:36 -0800

    Minor cleanup of HF_GenWeightsTest.java

--- src/HF_GenWeightsTest.java
+++ src/HF_GenWeightsTest.java
@@ -15,11 +15,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(OrderAnnotation.class)
 class HF_GenWeightsTest {
-
+	private final int ordCR = 13;
 	HuffCompTestLib hflib = new HuffCompTestLib();
 	GenWeights gw;
 	HuffCompAlerts hca;
-    private static boolean ignoreChr13 = false; 
     String dir = "data/";
     
     /**
@@ -40,12 +39,6 @@ class HF_GenWeightsTest {
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		System.out.println("Running on: "+getOperatingSystem());
-		ignoreChr13 = !(getOperatingSystem().contains("Win"));
-		if (ignoreChr13) {
-			System.out.println("Non-Windows platform detected; setting ignoreChr13="+ignoreChr13);			
-		} else {
-			System.out.println("Windows platform detected; setting ignoreChr13="+ignoreChr13);			
-		}
 	}
 
 	/**
@@ -77,6 +70,13 @@ class HF_GenWeightsTest {
 
 	/**
 	 * Test GenWeights on simple.txt
+	 * Each of the tests in this section uses the same methodology:
+	 * - use gw.readInputFileAndReturnWeights to generate the tstWeights array
+	 * - compares the tstWeights array against expectations using hflib.checkWeights,
+	 *   a method in HuffCompTestLib.jar. Checking distinguishes between Windows and
+	 *   Mac/Linux/Unix file systems based upon the weight of the CR character (ordinal
+	 *   value of 13) - Windows stores CR with every LF (ordinal value 10 - a new line), 
+	 *   whereas Mac/Linux/Unix do not use CR.
 	 */
 	@Test
 	@Order(1)
@@ -85,7 +85,7 @@ class HF_GenWeightsTest {
 		gw = new GenWeights(hca);
 		
 		int[] tstWeights = gw.readInputFileAndReturnWeights(dir+"simple.txt");
-		boolean ignCHR13 = (tstWeights[13] == 0);
+		boolean ignCHR13 = (tstWeights[ordCR] == 0);
 		assertTrue(hflib.checkWeights("simple.txt", ignCHR13, tstWeights));
 	}
 	
@@ -99,7 +99,7 @@ class HF_GenWeightsTest {
 		gw = new GenWeights(hca);
 		
 		int[] tstWeights = gw.readInputFileAndReturnWeights(dir+"Green Eggs and Ham.txt");
-		boolean ignCHR13 = (tstWeights[13] == 0);
+		boolean ignCHR13 = (tstWeights[ordCR] == 0);
 		assertTrue(hflib.checkWeights("Green Eggs and Ham.txt", ignCHR13, tstWeights));
 	}
 
@@ -113,7 +113,7 @@ class HF_GenWeightsTest {
 		gw = new GenWeights(hca);
 		
 		int[] tstWeights = gw.readInputFileAndReturnWeights(dir+"The Cat in the Hat.txt");
-		boolean ignCHR13 = (tstWeights[13] == 0);
+		boolean ignCHR13 = (tstWeights[ordCR] == 0);
 		assertTrue(hflib.checkWeights("The Cat in the Hat.txt", ignCHR13, tstWeights));
 	}
 
@@ -127,7 +127,7 @@ class HF_GenWeightsTest {
 		gw = new GenWeights(hca);
 		
 		int[] tstWeights = gw.readInputFileAndReturnWeights(dir+"Harry Potter and the Sorcerer.txt");
-		boolean ignCHR13 = (tstWeights[13] == 0);
+		boolean ignCHR13 = (tstWeights[ordCR] == 0);
 		assertTrue(hflib.checkWeights("Harry Potter and the Sorcerer.txt", ignCHR13, tstWeights));
 	}
 
@@ -141,7 +141,7 @@ class HF_GenWeightsTest {
 		gw = new GenWeights(hca);
 		
 		int[] tstWeights = gw.readInputFileAndReturnWeights(dir+"warAndPeace.txt");
-		boolean ignCHR13 = (tstWeights[13] == 0);
+		boolean ignCHR13 = (tstWeights[ordCR] == 0);
 		assertTrue(hflib.checkWeights("warAndPeace.txt", ignCHR13, tstWeights));
 	}
 
