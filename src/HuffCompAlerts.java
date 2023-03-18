0c972fbc70beafdbf80e70d62696ac2d44771602
smurray-mvla <scott.murray@mvla.net>
2023-03-18 16:23:07 -0700

    updating HuffCompAlerts to support Error Checking

--- src/HuffCompAlerts.java
+++ src/HuffCompAlerts.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * This class is provided to insulate the EncodeDecode and GenWeights classes from
@@ -10,7 +11,10 @@
 public class HuffCompAlerts {
 	
 	/** The gui. */
-	private HuffCompGUI gui;
+	private HuffCompGUI gui;	
+	
+	/** The last alert type - used by the JUnit to ensure that alerts are appropriately tested. */
+	private ArrayList<String> lastAlertType;
 	
 	/**
 	 * Constructor for the HuffCompAlerts class. Connects the class to the GUI
@@ -19,6 +23,7 @@ public class HuffCompAlerts {
 	 */
 	public HuffCompAlerts (HuffCompGUI gui) {
 		this.gui = gui;
+		lastAlertType = new ArrayList<String>();
 	}
 
 	/**
@@ -26,10 +31,14 @@ public class HuffCompAlerts {
 	 * message to the appropriate alert method in the GUI. If the GUI is null,
 	 * outputs the hdr and message to the Console.
 	 *
-	 * @param alert the alert
-	 * @param hdr the hdr
-	 * @param message the message
-	 * @return true, if is sue alert
+	 * @param alert the alert type
+	 * @param hdr the header message (a JavaFX Alert has both a header to indicate
+	 *            general information ("Input Error") and a message that provides 
+	 *            the details
+	 * @param message the detailed error message
+	 * @return true, with the exception of GUI enabled CONFIRM alerts, in which
+	 *            case it returns true if the user clicked OK and false if the user
+	 *            clicked Cancel.
 	 */
 	boolean issueAlert(HuffAlerts alert, String hdr, String message) {
 		boolean result = true;
@@ -42,8 +51,24 @@ public class HuffCompAlerts {
 			}
 		} else {
 			System.out.println("HuffCompAlert: "+hdr+" ==> "+message);
+			lastAlertType.add(alert.name());
 		}
 		return result;
 	}
+	
+	/**
+	 * Gets the ArrayList of all recorded alerts.
+	 *
+	 * @return the last alert type
+	 */
+	ArrayList<String> getLastAlertType() {
+		return lastAlertType;
+	}
 
+	/**
+	 * Clears the lastAlertType ArrayList.
+	 */
+	void resetLastAlertType() {
+		lastAlertType.clear();
+	}
 }
